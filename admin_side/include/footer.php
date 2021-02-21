bd80b95d5c02e514f3c3d88c351e074947912b9c
__Brinda_Raycha__ <76004193+BrindaRaycha@users.noreply.github.com>
2021-02-21 13:18:40 +0530

    Add files via upload

---
+++ admin_side/include/footer.php
@@ -0,0 +1,10 @@
+<nav class="layout-footer footer footer-light">
+<div class="container-fluid d-flex flex-wrap justify-content-between text-center container-p-x pb-3">
+<div class="pt-3">
+<span class="float-md-right d-none d-lg-block">&copy; Exclusive on Rattle n Roll | Hand-crafted &amp; Made with <i class="fas fa-heart text-danger mr-2"></i></span>
+</div>
+<div>
+<a href="javascript:" class="footer-link pt-3 ml-4">Developed By Brinda &amp; Vidhi</a>
+</div>
+</div>
+</nav>
---
+++ admin_side/include/side-bar.php
@@ -0,0 +1,237 @@
+<div id="layout-sidenav" class="layout-sidenav sidenav sidenav-vertical bg-white logo-dark">
+
+<div class="app-brand demo">
+<span class="app-brand-logo demo">
+<img src="assets/img/a_logo.jpeg" alt="Brand Logo" class="img-fluid" style="
+    width: 150px;
+    height: 80px;
+    
+">
+</span>
+
+<a href="javascript:" class="layout-sidenav-toggle sidenav-link text-large ml-auto">
+<i class="ion ion-md-menu align-middle"></i>
+</a>
+</div>
+<div class="sidenav-divider mt-0"></div>
+
+<ul class="sidenav-inner py-1">
+
+
+
+
+
+<li class="sidenav-header small font-weight-semibold">Product & Category</li>
+<li class="sidenav-item">
+<a href="javascript:" class="sidenav-link sidenav-toggle">
+<i class="sidenav-icon feather icon-airplay"></i>
+<div>Category</div>
+</a>
+<ul class="sidenav-menu">
+<li class="sidenav-item">
+<a href="product_category_add.php" class="sidenav-link" >
+<div>Category Add</div>
+</a>
+</li>
+<li class="sidenav-item">
+<a href="product_category_list.php" class="sidenav-link" >
+<div>Category List</div>
+</a>
+</li>
+</ul>
+</li>
+
+
+
+<li class="sidenav-item">
+<a href="javascript:" class="sidenav-link sidenav-toggle">
+<i class="sidenav-icon feather icon-box"></i>
+<div>Product</div>
+</a>
+<ul class="sidenav-menu">
+
+<li class="sidenav-item">
+<a href="product_list.php" class="sidenav-link">
+<div>Product List</div>
+</a>
+</li>
+</ul>
+</li>
+
+
+<li class="sidenav-item">
+<a href="javascript:" class="sidenav-link sidenav-toggle">
+<i class="sidenav-icon fas fa-coins"></i>
+<div>Payment</div>
+</a>
+<ul class="sidenav-menu">
+<li class="sidenav-item">
+<a href="payment_type_add.php" class="sidenav-link" >
+<div>Payment Type Add</div>
+</a>
+</li>
+<li class="sidenav-item">
+<a href="payment_type_list.php" class="sidenav-link" >
+<div>Payment Type List</div>
+</a>
+</li>
+</ul>
+</li>
+
+<li class="sidenav-divider mb-1"></li>
+<li class="sidenav-header small font-weight-semibold">Brand</li>
+<li class="sidenav-item active">
+<a href="javascript:" class="sidenav-link sidenav-toggle">
+<i class="sidenav-icon ion ion-logo-buffer"></i>
+<div>Brand</div>
+</a>
+<ul class="sidenav-menu">
+<li class="sidenav-item">
+<a href="brand_add.php" class="sidenav-link" >
+<div>Brand Add</div>
+</a>
+</li>
+<li class="sidenav-item">
+<a href="brand_list.php" class="sidenav-link" >
+<div>Brand List</div>
+</a>
+</li>
+</ul>
+</li>
+
+
+
+<li class="sidenav-divider mb-1"></li>
+<li class="sidenav-header small font-weight-semibold">Vendor & User</li>
+<li class="sidenav-item active">
+<a href="javascript:" class="sidenav-link sidenav-toggle">
+<i class="sidenav-icon  fas fa-address-card"></i>
+<div>Vendors</div>
+</a>
+<ul class="sidenav-menu">
+<li class="sidenav-item">
+<a href="vendor_list.php" class="sidenav-link">
+<div>Vendor List</div>
+</a>
+</li>
+<li class="sidenav-item">
+<a href="vendor_blocked_list.php" class="sidenav-link">
+<div>Blocked Vendor</div>
+</a>
+</li>
+</ul>
+</li>
+<li class="sidenav-item">
+<a href="javascript:" class="sidenav-link sidenav-toggle">
+<i class="sidenav-icon feather icon-users"></i>
+<div>User</div>
+</a>
+<ul class="sidenav-menu">
+<li class="sidenav-item">
+<a href="User_Table.php" class="sidenav-link">
+<div>User List</div>
+</a>
+</li>
+<li class="sidenav-item">
+<a href="User_Blocked_List.php" class="sidenav-link">
+<div>Blocked User</div>
+</a>
+</li>
+</ul>
+</li>
+
+
+<li class="sidenav-divider mb-1"></li>
+<li class="sidenav-header small font-weight-semibold">State & City</li>
+<li class="sidenav-item">
+<a href="javascript:" class="sidenav-link sidenav-toggle">
+<i class="sidenav-icon ion ion-ios-keypad"></i>
+<div>State</div>
+</a>
+<ul class="sidenav-menu">
+<li class="sidenav-item">
+<a href="state_add.php" class="sidenav-link" >
+<div>State Add</div>
+</a>
+</li>
+<li class="sidenav-item">
+<a href="state_list.php" class="sidenav-link" >
+<div>State List</div>
+</a>
+</li>
+</ul>
+</li>
+
+<li class="sidenav-item">
+<a href="javascript:" class="sidenav-link sidenav-toggle">
+<i class="sidenav-icon ion ion-ios-keypad"></i>
+<div>City</div>
+</a>
+<ul class="sidenav-menu">
+<li class="sidenav-item">
+<a href="city_add.php" class="sidenav-link" >
+<div>City Add</div>
+</a>
+</li>
+<li class="sidenav-item">
+<a href="city_list.php" class="sidenav-link" >
+<div>City List</div>
+</a>
+</li>
+</ul>
+</li>
+
+
+<li class="sidenav-divider mb-1"></li>
+<li class="sidenav-header small font-weight-semibold">Order</li>
+<li class="sidenav-item">
+<a href="javascript:" class="sidenav-link sidenav-toggle">
+<i class="sidenav-icon ion ion-md-paper"></i>
+<div>Order</div>
+</a>
+<ul class="sidenav-menu">
+
+
+<li class="sidenav-item">
+<a href="order_list.php" class="sidenav-link">
+<div>Order List</div>
+</a>
+</li>
+</ul>
+</li>
+
+<li class="sidenav-divider mb-1"></li>
+<li class="sidenav-header small font-weight-semibold">Career</li>
+<li class="sidenav-item active">
+<a href="javascript:" class="sidenav-link sidenav-toggle">
+<i class="sidenav-icon  feather icon-user-plus"></i>
+<div>Applicant</div>
+</a>
+<ul class="sidenav-menu">
+<li class="sidenav-item">
+<a href="career.php" class="sidenav-link">
+<div>Applicant List</div>
+</a>
+</li>
+</ul>
+</li>
+
+<li class="sidenav-divider mb-1"></li>
+<li class="sidenav-header small font-weight-semibold">Contact Us</li>
+<li class="sidenav-item active">
+<a href="javascript:" class="sidenav-link sidenav-toggle">
+<i class="sidenav-icon  feather icon-phone-incoming"></i>
+<div>Contact Us</div>
+</a>
+<ul class="sidenav-menu">
+<li class="sidenav-item">
+<a href="contact_us.php" class="sidenav-link">
+<div>Contact us</div>
+</a>
+</li>
+</ul>
+</li>
+
+
+</ul>
+</div>
